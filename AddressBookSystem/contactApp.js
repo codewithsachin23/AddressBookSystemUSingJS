4f35ee8e83fb20fe8309a58744abd102a55b9fb2
sachin shukla <sachinshukla232003@gmail.com>
2025-03-04 11:14:54 +0530

    [Sachin SHukla]Add. UC-2 Contact validation and throw error

--- AddressBookSystem/contactApp.js
+++ AddressBookSystem/contactApp.js
@@ -1,5 +1,15 @@
 class Contact{
     constructor(firstname,lastname,address, city, state, zip, phonenumber,email){
+       if(!this.isValidName(firstname)) throw new Error("First character must be Capital and minimum 3 character");
+       if(!this.isValidName(lastname))throw new Error("First character must be Capital and minimum 3 character");
+       if(!this.isValidAddress(address))throw new Error("must be minimum 4 character");
+       if(!this.isValidAddress(city))throw new Error("must be minimum 4 character");
+       if(!this.isValidAddress(state))throw new Error("must be minimum 4 character");
+       if(!this.isValidpin(zip))throw new Error("must be 6 digit");
+       if(!this.isValidNumber(phonenumber))throw new Error("phone number must be 10 digit And Start from 6 to 9");
+       if(!this.isValidGmail(email))throw new Error("writen valid email");
+
+     
         this.firstName=firstname;
         this.lastname=lastname;
         this.address=address;
@@ -13,8 +23,33 @@ class Contact{
     toString(){
         return `${this.firstName},${this.lastname},${this.address},${this.city},${this.state},${this.zip},${this.phonenumber},${this.email},`;
     }
+
+   isValidName(name) {
+    let nameRegex=/^[A-Z][a-zA-Z]{2,}$/;
+    return nameRegex.test(name);
+    } 
+    
+    isValidAddress(address) {
+    let addressRegex=/^[a-zA-Z0-9\s]{4,}$/;
+    return addressRegex.test(address);
+    } 
+
+    isValidpin(pin){
+        let pinRegex=/^\d{6}$/;
+        return pinRegex.test(pin);
+    }
+    
+    isValidNumber(phonenumber){
+        let phonenumberRegex=/^[6789]\d{9}$/;
+        return phonenumberRegex.test(phonenumber);
+    }
+    isValidGmail(gmail){
+        let gmailRegex=/^[a-zA-Z0-9.+-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return gmailRegex.test(gmail);
+    }
+
 }
 
-let contact1=new Contact("sachin","shukla","anand Nagar Bhopal","Bhopal","madhya pradesh","462022","787926.....","sachinshukla@gmail.com");
+let contact1=new Contact("Sachin","Shukla","anand Nagar Bhopal","Bhopal","madhya pradesh",462022,7879268756,"sachinshukla@gmail.com");
 console.log(contact1);
 console.log(contact1.toString());
